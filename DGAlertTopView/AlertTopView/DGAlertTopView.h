b7ae546a7603776ca196b0e733a863102e699bcc
zhongweidi <Cody@CodydeMacBook-Pro.local>
2017-11-28 18:06:15 +0800

    搭建控件

---
+++ DGAlertTopView/AlertTopView/DGAlertTopView.h
@@ -0,0 +1,13 @@
+//
+//  DGAlertTopView.h
+//  DGAlertTopView
+//
+//  Created by Cody on 2017/11/27.
+//  Copyright © 2017年 Cody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DGAlertTopView : UIView
+
+@end
---
+++ DGAlertTopView/AlertTopView/DGAlertTopView.m
@@ -0,0 +1,21 @@
+//
+//  DGAlertTopView.m
+//  DGAlertTopView
+//
+//  Created by Cody on 2017/11/27.
+//  Copyright © 2017年 Cody. All rights reserved.
+//
+
+#import "DGAlertTopView.h"
+
+@implementation DGAlertTopView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
